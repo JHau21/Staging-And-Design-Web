527b43140b78979aa6646da2a8a481e2c6381fe0
cnr-dxn <connorddixon@gmail.com>
2021-06-11 15:44:47 -0600

    Added basic html to home page

--- momswebsite/src/App.js
+++ momswebsite/src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.css"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="masterOuterContainer" className={styles.masterOuterContainer}> 
+      <div id="innerPage" className={styles.innerSomethingElse}>
+        <header>
+          <title>Simply Stated</title>
+          <link rel="icon" href="/finalLogoDark.png" />
+          <link
+            // rel="stylesheet"
+            // href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+            // integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
+            // crossorigin="anonymous"
+          />
+        </header>
+        <div id="masterNavBar" className={styles.masterNavBar}>
+          <ul>
+
+            <li><img src="/finalLogoDark.png" alt="Logo" width="100"></img></li>
+            <li>Home</li>
+            <li>Portfolio</li>
+            <li>Services</li>
+            <li>Schedule</li>
+            <li>About</li>
+            <li><img src="/linkedIn.png" alt="LinkedInLogo" width="100"></img></li> 
+          </ul>
+          <h1>
+            Give your home the meaning it deserves.
+          </h1>
+          <h3>
+            We help transform your space into the space of your dreams, and are with you every step of the way.
+          </h3>
+          <p>
+            This is where the button will go.
+          </p>
+        </div>
+        <div id="masterMainPanel" className={styles.masterMainPanel}>
+        </div>
+      </div>
+      <style jsx global>{`
+        @import url("https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap");
+        html,
+        body {
+          height: 100%;
+          margin: 0;
+          // border: 5px solid green;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+          font-family: "Nunito", sans-serif;
+          background-color: #50605E;
+          background-image: "/mainPanelBackground.png";
+          overflow: auto;
+        }
+      `}</style>
     </div>
   );
 }
---
+++ momswebsite/src/pages/testing.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+        <p>
+          Other shit
+        </p>
+    </div>
+  );
+}
+
+export default App;
