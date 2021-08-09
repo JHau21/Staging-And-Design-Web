a47542f798b2903ad9c49e3e248d020357020e4e
JHau21 <JackHau13@gmail.com>
2021-08-08 18:30:08 -0600

    Finished Navigation bar and routing

--- momswebsite/src/PageComponents/Navigation/NavigationBar/index.js
+++ momswebsite/src/PageComponents/Navigation/NavigationBar/index.js
@@ -1,29 +1,14 @@
-import React, { useEffect } from "react";
+import React from "react";
 import {
   BrowserRouter as Router,
   Link,
   useHistory,
   useLocation,
 } from "react-router-dom";
+import classnames from "classnames";
 
 import { theme } from "../../../Theme/index";
 import styles from "./index.module.css";
-
-// Note to future Jack: Add an array the maintains all the props passed to this nav bar and map the array passing object items to this link component?
-const BasicLink = ({ name, link, component } = props) => {
-  return (
-    <div className={styles.linkContainer}>
-      <Link
-        style={fontGeneral}
-        to={"/Home"}
-        className={styles.linkText}
-        onClick={() => history.push("/Home")}
-      >
-        Home
-      </Link>
-    </div>
-  );
-};
 
 // The style and layout of the actual navigation bar
 const NavigationBar = () => {
@@ -35,56 +20,67 @@ const NavigationBar = () => {
   const history = useHistory();
   const location = useLocation();
 
-  useEffect(() => {}, [location]);
+  const determineStyle = (route, isContainer = false) => {
+    if (isContainer) {
+      return classnames(
+        styles.linkContainer,
+        location.pathname === route ? styles.yellowBar : styles.whiteBar
+      );
+    }
+    return classnames(
+      styles.linkText,
+      location.pathname === route ? styles.yellow : styles.white
+    );
+  };
 
   return (
     <div className={styles.root}>
       <Router>
-        <div className={styles.linkContainer}>
+        <div className={determineStyle("/Home", true)}>
           <Link
             style={fontGeneral}
             to={"/Home"}
-            className={styles.linkText}
+            className={determineStyle("/Home")}
             onClick={() => history.push("/Home")}
           >
             Home
           </Link>
         </div>
-        <div className={styles.linkContainer}>
+        <div className={determineStyle("/Portfolio", true)}>
           <Link
             style={fontGeneral}
             to={"/Portfolio"}
-            className={styles.linkText}
+            className={determineStyle("/Portfolio")}
             onClick={() => history.push("/Portfolio")}
           >
             Portfolio
           </Link>
         </div>
-        <div className={styles.linkContainer}>
+        <div className={determineStyle("/Services", true)}>
           <Link
             style={fontGeneral}
             to={"/Services"}
-            className={styles.linkText}
+            className={determineStyle("/Services")}
             onClick={() => history.push("/Services")}
           >
             Services
           </Link>
         </div>
-        <div className={styles.linkContainer}>
+        <div className={determineStyle("/Schedule", true)}>
           <Link
             style={fontGeneral}
             to={"/Schedule"}
-            className={styles.linkText}
+            className={determineStyle("/Schedule")}
             onClick={() => history.push("/Schedule")}
           >
             Schedule
           </Link>
         </div>
-        <div className={styles.linkContainer}>
+        <div className={determineStyle("/About", true)}>
           <Link
             style={fontGeneral}
             to={"/About"}
-            className={styles.linkText}
+            className={determineStyle("/About")}
             onClick={() => history.push("/About")}
           >
             About
