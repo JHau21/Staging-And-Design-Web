498c0dabba8548b3e292a659529993f8cad00e94
JHau21 <JackHau13@gmail.com>
2021-08-15 16:00:54 -0600

    More button general styling changes

--- momswebsite/src/CommonComponents/Button/index.js
+++ momswebsite/src/CommonComponents/Button/index.js
@@ -1,11 +1,28 @@
 import React from "react";
 
+import { theme } from "../../Theme/index";
 import styles from "index.module.css";
 
-const Button = ({ color }) => {
-    return (
-        
-    );
+const Button = ({
+  color,
+  text,
+  textSize,
+  fontFamily = "semiBold",
+  className,
+  onClick,
+}) => {
+  return (
+    <button
+      className={className ? className : styles.root}
+      style={{
+        color: color ? color : theme.palette.yellow,
+        fontSize: textSize ? textSize : theme.fontSize.medium,
+        ...theme.font.fontFamily,
+      }}
+      text={text}
+      onClick={onClick()}
+    />
+  );
 };
 
 export default Button;
